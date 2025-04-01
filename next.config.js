42943518f9d7debed528d5dcea435725fa6f88a6
Sukhmander Singh <callmesukhi@github.com>
2025-04-01 22:20:09 +0200

    fix actions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '', // keep empty for user/organization pages
+  assetPrefix: './' // fix paths on GitHub Pages
 };
 
 module.exports = nextConfig;
