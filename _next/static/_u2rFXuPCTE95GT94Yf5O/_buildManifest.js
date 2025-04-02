ea339623f435e0be21050bdffaee749e76e93e40
callmesukhi <callmesukhi@users.noreply.github.com>
2025-04-02 00:16:16 +0000

    deploy: e982daab9518a68b20a3f9ddd5348ff05c31e8c1

---
+++ _next/static/_u2rFXuPCTE95GT94Yf5O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/425-0d8eb46263525292.js","static/chunks/pages/index-787fa641472e488b.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
